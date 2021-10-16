a478d6fbed84482c28cb3b2c9ae633d8870392f4
OljasIzteleuov <adat121205@gmail.com>
2021-10-16 12:08:18 +0500

    olzhas

---
+++ exercise2.py
@@ -0,0 +1,15 @@
+print("Введите цену за 1кг конфеты: ")
+confety = int(input())
+
+print("Введите цену за 1кг печенье: ")
+pechenya = int(input())
+
+print("Цены сохранены!")
+a=confety*0.3+pechenya*0.4
+print("a=",a)
+b=(2*pechenya+1.8*confety)*3
+print("b=",b)
+
+
+
+
